223de45c0db93c152c53cd2753f5f3277c45bc87
Kauê Wendt Sabino <kaue.sabino@modalgr.com.br>
2024-01-10 14:53:02 -0300

    feat: Conectando com o MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+//string de conexao: mongodb+srv://usuario_admin:<password>@clusterapi.9dnc6k6.mongodb.net/?retryWrites=true&w=majority
+
+//CONFIGURAÇÕES DO MONGODB
+const url = 'mongodb+srv://usuario_admin:Kaue132302@clusterapi.9dnc6k6.mongodb.net/?retryWrites=true&w=majority';
+const options = {useNewUrlParser: true};
+
+mongoose.connect(url, options);
+// mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err)  => {
+    console.log('Erro na conexão com o banco de dados:' + err);
+})
+
+mongoose.connection.on('disconnected', (err)  => {
+    console.log('Aplicação desconectada do banco de dados:');
+})
+
+mongoose.connection.on('connected', (err)  => {
+    console.log('Aplicação conectada do banco de dados:');
+})
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//ROTAS
 const indexRoute = require('./Routes/index');
 const usersRoute = require('./Routes/users');
 
 app.use('/', indexRoute);
 app.use('/users', usersRoute);
-
 
 app.listen(3000); //porta de conexão da API
 
