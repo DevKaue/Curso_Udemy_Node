7925dc10437e6377b258f0397d6bc870ca31f467
Kaue <kauesabino@hotmail.com>
2025-04-27 23:34:15 -0300

    fix: atualizando config.js

--- Config/config.js
+++ Config/config.js
@@ -7,20 +7,21 @@ const config = () => {
                 bd_string: 'mongodb+srv://kauesabino:gQlcCJ8YHSdCi1JB@cluster0.crjvegz.mongodb.net/',
                 jwt_pass: 'kahnsbvfkanbfuyagb2974y234973264',
                 jwt_expires: '7d'
-        }
+            };
         case 'hml':
             return {
                 bd_string: 'mongodb+srv://kauesabino:gQlcCJ8YHSdCi1JB@cluster0.crjvegz.mongodb.net/',
                 jwt_pass: '',
                 jwt_expires: ''
-
-        }
+            };
         case 'prod':
             return {
                 bd_string: 'mongodb+srv://kauesabino:gQlcCJ8YHSdCi1JB@cluster0.crjvegz.mongodb.net/',
                 jwt_pass: 'kahnsbvfkanbfuyagb2974yçaojflha7gahl8g3flag7faw7fg3lf',
                 jwt_expires: '7d'
-        }
+            };
+        default:
+            throw new Error(`Ambiente '${env}' não reconhecido.`);
     }
 }
 
